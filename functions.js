eae54001cfadc857472d0c6f3a9a71ebf2c822b4
Andrea Roa <andrearoa182@gmail.com>
2023-06-15 22:12:26 -0500

    se realiza funcion que valide si el directorio tiene archivos

--- functions.js
+++ functions.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const promise = require('fs/promises');
 const path = require ('path');
+//const markdownIt = require('markdown-it')
+
 //validar ruta si es absoluta
 const isAbsolute=(route)=>path.isAbsolute(route);
 //Convertir ruta relativa a absoluta
@@ -13,16 +15,38 @@ const isAFile = (route) =>fs.statSync(route).isFile();
 //es un directorio
 const isADirectory =(route) =>fs.statSync(route).isDirectory();
 // leer archivos de un directorio
-const readDirectory=(route)=>fs.readdirSync(route, 'utf8');
-//validar si tiene extension .md
-const isMd = (route)=> {
-const result= path.extname(route)
-if (result==='.md'){
-console.log(true);
+// validar archivos si hay 
+// si hay , iterar por cada uno 
+//volver el archivo en ruta absoluta 
+// ir uniendo por .push las rutas de los archivos del directorio .
+const readDirectory=(route)=>{
+const generalRead= fs.readdirSync(route, 'utf8');
+const arrayRoute = [];
+if (generalRead){
+generalRead.forEach((route)=>{
+resultExt= path.extname(route);
+if(resultExt === '.md'){
+const routeAbsolute= path.resolve(route);
+arrayRoute.push(routeAbsolute);
 }
-else{console.log(null);}
-}
+else{return null;}
+})}
+console.log(arrayRoute);
+};
 
-isMd("C:/Users/wader/Documents/DEV006-md-links/index.js");
+readDirectory("C:/Users/wader/Documents/DEV006-md-links/prueba");
+
+
+// //validar si tiene extension .md
+// const isMd = (route)=> {
+// const result= path.extname(route)
+// if (result==='.md'){
+
+// console.log(true);
+// }
+// else{console.log(null);}
+// }
+
+
 
 
