1c6de3bfc70ea963ccda49dead7b9b4eb6b030f7
Andrea Roa <andrearoa182@gmail.com>
2023-06-16 00:00:45 -0500

    se agrega la funcion que lee el directorio, valida rutas y las que terminan en .md las vuelve rutas absolutas

--- functions.js
+++ functions.js
@@ -1,40 +1,69 @@
-const fs = require('fs');
-const promise = require('fs/promises');
-const path = require ('path');
+const { error } = require("console");
+const fs = require("fs");
+const promise = require("fs/promises");
+const path = require("path");
 //const markdownIt = require('markdown-it')
 
 //validar ruta si es absoluta
-const isAbsolute=(route)=>path.isAbsolute(route);
+const isAbsolute = (route) => path.isAbsolute(route);
 //Convertir ruta relativa a absoluta
-const converterAbsolute=(route)=>path.resolve(route);
-//existe la ruta 
-const existPath=(route)=>fs.existsSync(route);
+const converterAbsolute = (route) => path.resolve(route);
+//existe la ruta
+const existPath = (route) => fs.existsSync(route);
 //es un archivo
-const isAFile = (route) =>fs.statSync(route).isFile();
+const isAFile = (route) => fs.statSync(route).isFile();
 
 //es un directorio
-const isADirectory =(route) =>fs.statSync(route).isDirectory();
+const isADirectory = (route) => fs.statSync(route).isDirectory();
 // leer archivos de un directorio
-// validar archivos si hay 
-// si hay , iterar por cada uno 
-//volver el archivo en ruta absoluta 
+// validar archivos si hay
+// si hay , iterar por cada uno
+//volver el archivo en ruta absoluta
 // ir uniendo por .push las rutas de los archivos del directorio .
-const readDirectory=(route)=>{
-const generalRead= fs.readdirSync(route, 'utf8');
-const arrayRoute = [];
-if (generalRead){
-generalRead.forEach((route)=>{
-resultExt= path.extname(route);
-if(resultExt === '.md'){
-const routeAbsolute= path.resolve(route);
-arrayRoute.push(routeAbsolute);
-}
-else{return null;}
-})}
-console.log(arrayRoute);
+const readDirectory = (route) => {
+  return new Promise((resolve, reject) => {
+    const generalRead = fs.readdir(route, "utf8", (error, routes) => {
+      if (error) {
+        reject(error);
+      } else {
+        const arrayRoute = [];
+        routes.forEach((file) => {
+          const resultExt = path.extname(file);
+          if (resultExt === ".md") {
+            const routeAbsolute = path.resolve(file);
+            arrayRoute.push(routeAbsolute);
+          }
+        });
+        resolve(arrayRoute);
+      }
+    });
+  });
 };
 
-readDirectory("C:/Users/wader/Documents/DEV006-md-links/prueba");
+readDirectory("C:/Users/wader/Documents/DEV006-md-links/index.js")
+.then((arrayRoute)=>{
+console.log('las rutas son:',arrayRoute);
+}).catch((error)=>{
+console.log('se genero un error',error);
+})
+
+// const readDirectory = (route) => {
+//   const generalRead = fs.readdirSync(route, "utf8");
+//   const arrayRoute = [];
+//   if (generalRead) {
+//     generalRead.forEach((route) => {
+//       resultExt = path.extname(route);
+//       if (resultExt === ".md") {
+//         const routeAbsolute = path.resolve(route);
+//         arrayRoute.push(routeAbsolute);
+//       } else {
+//         return null;
+//       }
+//     });
+//   }
+//   console.log(arrayRoute);
+// };
+
 
 
 // //validar si tiene extension .md
@@ -46,7 +75,3 @@ readDirectory("C:/Users/wader/Documents/DEV006-md-links/prueba");
 // }
 // else{console.log(null);}
 // }
-
-
-
-
